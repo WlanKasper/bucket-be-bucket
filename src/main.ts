e34a6f7e399584b1470638242975a1a4c5f49d1e
Artur Smirnov <artur-smirnov@dedagroup.it>
2024-09-10 19:27:27 +0200

    GCS deployment

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const bootstrap = async () => {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   const configService = app.get<ConfigService>(ConfigService);
-  const port = configService.get<string>('PORT') || 3001;
+  const port = configService.get<string>('PORT') || 8080;
 
   await app.listen(port);
 };
