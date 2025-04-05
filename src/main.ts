f3e4d5022625c15fd151cab4b6d3cc8b49e9a766
Artur Smirnov <artur-smirnov@dedagroup.it>
2025-04-06 00:13:44 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  const port = 80;
+  const port = 8080;
 
   await app.listen(port);
 };
